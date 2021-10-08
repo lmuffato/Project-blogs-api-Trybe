fa4f32fe56852640acf2ab6d94ba4e6cb1cb4c32
AdelinoJnr <adelinojunior96@gmail.com>
2021-10-08 18:39:44 -0300

    feat: controller users

---
+++ controllers/users.js
@@ -0,0 +1,47 @@
+const Users = require('../services/users.js');
+
+const getAll = async (req, res) => {
+  const { status, data, message } = await Users.getAll();
+  if (message) return res.status(status).json(message);
+
+  res.status(status).json(data);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const { status, data, message } = await Users.getById(id);
+  if (message) return res.status(status).json(message);
+
+  res.status(status).json(data);
+};
+
+const create = async (req, res) => {
+  const { status, data, message } = await Users.create(req.body);
+  if (message) return res.status(status).json(message);
+
+  res.status(status).json(data);
+};
+
+const remove = async (req, res) => {
+  const { id } = req.params;
+  const { status, data, message } = await Users.remove(id);
+  if (message) return res.status(status).json(message);
+
+  res.status(status).json(data);
+};
+
+const update = async (req, res) => {
+  const { id } = req.params;
+  const { status, data, message } = await Users.update(id, req.body);
+  if (message) return res.status(status).json(message);
+
+  res.status(status).json(data);
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  remove,
+  update,
+};
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
