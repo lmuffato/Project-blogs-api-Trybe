376bd5ae4e74a65a7bfb3cc462734495254e8cde
RenzoSev <sevilharenzo@gmail.com>
2021-10-09 18:51:11 -0300

    feat: add models

---
+++ src/models/PostsCategory.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, _DataTypes) => {
+  const PostsCategory = sequelize.define(
+    'PostsCategory', {}, { timestamps: false },
+  );
+
+  PostsCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'post',
+      through: PostsCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostsCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  };
+
+  return PostsCategory;
+};
