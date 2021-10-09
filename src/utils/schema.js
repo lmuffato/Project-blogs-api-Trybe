1d59f73b33e8aed1e7a199942447d417ef31f006
AdelinoJnr <adelinojunior96@gmail.com>
2021-10-09 20:40:31 -0300

    fix: schema User(field password) req01

--- src/utils/schema.js
+++ src/utils/schema.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const User = Joi.object({
   displayName: Joi.string().required().min(8),
   email: Joi.string().email().required(),
-  password: Joi.string().required().min(6),
+  password: Joi.string().required().length(6),
   image: Joi.string(),
 });
 
