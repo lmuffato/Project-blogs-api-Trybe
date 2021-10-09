50fad73a353981400722753c14e22477193f299b
AdelinoJnr <adelinojunior96@gmail.com>
2021-10-09 18:05:42 -0300

    feat: listagem dos posts req08

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,22 +1,27 @@
-const { Post, User } = require('../models');
+const { Post, User, Category } = require('../models');
 const Schema = require('../utils/schema');
 
-const create = async (data, { id: userId }) => {
+const create = async (data, { id: _userId }) => {
   const { error } = Schema.Posts.validate(data);
   if (error) return { status: 400, message: error.details[0].message };
   
-  const { categoryIds, title, content } = data;
-  console.log(userId);
-  const post = await Post.create({ title, content, userId });
-  console.log(post);
+  // const { categoryIds, title, content } = data;
+  // console.log(userId);
+  // const post = await Post.create({ title, content, userId });
+  // console.log(post);
   
-  return { status: 201, data: post };
+  return { status: 201, data: 1 };
 };
 
 const getAll = async () => {
-  const posts = await Post.findAll();
+  const posts = await Post.findAll({ 
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
 
-  return { status: 200, data: [posts[0].dataValues] };
+  return { status: 200, data: posts };
 };
 
 const getById = async (id) => {
