37f134409d80779d4b424307790ce8b6c8c75baf
Maria Baeta <m.martinsdg@gmail.com>
2021-10-10 12:01:05 -0300

    feat: :sparkles: Configurações iniciais terminadas

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
-
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const { PORT } = process.env;
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
