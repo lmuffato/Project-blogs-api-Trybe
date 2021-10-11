96b159ec7f68c24841eed50ddfbedd887f2fc5b6
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-10-11 15:00:05 -0400

    config create-PostCategories migration

--- migrations/20211011180531-create-PostsCategories.js
+++ migrations/20211011180531-create-PostsCategories.js
@@ -2,20 +2,18 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+    });
+
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('PostCategories');
   }
 };
