c04d01ccae06650baaf089eb1522cd08896bf98e
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-10-11 15:04:16 -0400

    config create-BlogPosts migration

--- migrations/20211011180558-create-BlogPosts.js
+++ migrations/20211011180558-create-BlogPosts.js
@@ -2,20 +2,27 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      // createdAt: {},
+      // updatedAt: {},
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('BlogPosts');
   }
 };
