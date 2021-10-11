32edaa8e4b90d646a166995fae68682ffb67c5b8
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-10-11 06:34:46 -0300

    change tokenGenerator

--- util/tokenGenerator.js
+++ util/tokenGenerator.js
@@ -9,7 +9,7 @@ const jwtConfig = {
 const secret = process.env.JWT_SECRET || '123456';
 
 const generateToken = (data) => {
-  const token = jwt.sign({ data }, SECRET, jwtConfig);
+  const token = jwt.sign({ data }, secret, jwtConfig);
   return token;
 };
 
