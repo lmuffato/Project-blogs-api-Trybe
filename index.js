5e66d53af65c44446179bc6397f760d92724a1ce
ScriptCamilo <script.camilo@gmail.com>
2021-10-11 17:32:46 -0300

    Initial commit :rocket:

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
@@ -7,4 +8,13 @@ app.listen(3000, () => console.log('ouvindo porta 3000!'));
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
+});
+
+app.use((err, _req, res, _next) => {
+  const { status, message } = err;
+  const statusCode = status || 500;
+
+  res.status(statusCode).json({
+    message,
+  });
 });
