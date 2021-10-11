4d45bdfbea6a3794b5fc2f224c699df70e0a2bb5
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-10-11 05:10:04 -0300

    resolve conflict part 2

--- models/user.js
+++ models/user.js
@@ -1,13 +1,14 @@
-'use strict';
-const User = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
+  },
+  {
+    tableName: 'Users',
+    timestamps: false,
   });
 
   return User;
 };
-
-module.exports = User;
