54ff49f8909632a19ea324b2451c09bdba19f1d0
ScriptCamilo <script.camilo@gmail.com>
2021-10-12 17:27:39 -0300

    refactor

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
+app.get('/', (_req, res) => {
+  res.send();
 });
 
 app.use((err, _req, res, _next) => {
