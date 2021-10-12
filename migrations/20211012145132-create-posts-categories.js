5becbda000e182c44a55b60e15415c74b4f3a3a4
iago <iago.pferreiravr@gmail.com>
2021-10-12 15:23:22 -0300

    Feat: Condiguracoes iniciais

---
+++ migrations/20211012145132-create-posts-categories.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostsCategories', {
+      postId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Categories',
+          key: 'id'
+        }
+      },
+      categoryId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'BlogPosts',
+          key: 'id'
+        }
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostsCategories');
+  }
+};
---
+++ models/category.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, DataTypes) => {
+  const category = sequelize.define('Category', {
+    name: DataTypes.STRING,
+  }, {
+    tableName: 'Categories',
+    timestamps: false,
+  });
+
+  return category;
+};
