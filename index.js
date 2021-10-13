9d5b95c09e7568782de61af178169cda0f9f9c4a
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-10-13 20:08:39 -0300

    main index file

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+const userRouter = require('./routers/userRouter');
+
+app.use('/user', userRouter);
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
