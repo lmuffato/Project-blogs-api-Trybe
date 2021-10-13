fc22882f6063a7c4b192cab64d18814e90d1367c
Joao Nasc <oi@joaonasc.dev>
2021-10-13 20:35:41 -0300

    feat: add user routes and json parser

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
+const userRoutes = require('./routes/user');
+
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
+
+app.use('/user', userRoutes);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
