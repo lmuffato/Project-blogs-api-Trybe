aef8faf9a6f19ed91ed20fe1253d575db7c2b816
lucas-paz <lucas.hpaz@outlook.com>
2021-10-13 19:36:42 -0300

    creates controller

---
+++ Controllers/userController.js
@@ -0,0 +1,18 @@
+const User = require('../Services/userService');
+const { builtError } = require('../Services/heplers');
+
+const addNew = async (req, res, next) => {
+  try {
+    const result = await User.create(req.body);
+    if (result.message) return next(result);
+
+    return res.status(201).json(result);
+  } catch (e) {
+    console.log(e.message);
+    next(builtError(500, 'Internal Error'));
+  }
+};
+
+module.exports = {
+  addNew,
+};
