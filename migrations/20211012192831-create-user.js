2d985b3a2368730e0ecf29b4439bddf3041d0747
ScriptCamilo <script.camilo@gmail.com>
2021-10-13 16:58:38 -0300

    feat: user associate hasMany blogPost

--- migrations/20211012192831-create-user.js
+++ migrations/20211012192831-create-user.js
@@ -19,7 +19,7 @@ module.exports = {
       },
       image: {
         type: Sequelize.STRING
-      },
+      }
     });
   },
   down: async (queryInterface, Sequelize) => {
--- models/user.js
+++ models/user.js
@@ -1,10 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
   }, { timestamps: false });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'user_id', as: 'blogposts' });
+  };
 
   return User;
 };
