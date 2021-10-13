1b49d939ec404c737a4304d559d30163f11ed476
Joao Nasc <oi@joaonasc.dev>
2021-10-13 19:07:21 -0300

    feat: create user migration

---
+++ migrations/20211013220100-User.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      
+      displayName: Sequelize.STRING,
+
+      email: {
+        unique: true,
+        type: Sequelize.STRING,
+      },
+
+      password: Sequelize.STRING,
+
+      image: Sequelize.STRING,
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  }
+};
