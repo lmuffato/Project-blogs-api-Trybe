1e45c44ca546007aac0e7e299025f9b353f5fdaf
lucas-paz <lucas.hpaz@outlook.com>
2021-10-13 19:37:43 -0300

    updates the errors middleware

--- Middlewares/errors.js
+++ Middlewares/errors.js
@@ -1,19 +1,9 @@
 const handleErrors = (err, _req, res, _next) => {
   if (err.isJoi) {
-    return res.status(422).json({
-      err: {
-        code: 'invalid_data',
-        message: err.details[0].message,
-      },
-    });
+    return res.status(400).json({ message: err.details[0].message });
   }
 
-  return res.status(err.code).json({
-    err: {
-      code: err.codeType,
-      message: err.message,
-    },
-  });
+  return res.status(err.code).json({ message: err.message });
 };
 
 module.exports = {
