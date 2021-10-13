ca2c3cd40963d8ffb12105f01d3c20a76e4ccddd
Joao Nasc <oi@joaonasc.dev>
2021-10-13 19:15:59 -0300

    feat: create blogposts migration

---
+++ migrations/20211013221118-BlogPost.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      title: Sequelize.STRING,
+      content: Sequelize.TEXT,
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      published: Sequelize.DATE, 
+      updated: Sequelize.DATE,
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('BlogPosts');
+  }
+};
