ec5cd91a34fa811203d50240b2d3a9974bbf7399
Joao Nasc <oi@joaonasc.dev>
2021-10-13 18:39:11 -0300

    feat: create blogpost model

---
+++ models/BlogPost.js
@@ -0,0 +1,20 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const blogPost = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, { timestamps: false });
+
+  blogPost.associate = (models) => {
+    blogPost.belongsTo(models.User, {
+      as: 'User',
+      foreignKey: 'userId',
+    });
+  };
+
+  return blogPost;
+};
+
+module.exports = BlogPost;
