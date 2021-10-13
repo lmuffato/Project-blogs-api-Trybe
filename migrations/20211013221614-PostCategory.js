94db57debeffb1057d191519aa5a4f578dfb43ad
Joao Nasc <oi@joaonasc.dev>
2021-10-13 19:19:22 -0300

    feat: create postcategory migration

---
+++ migrations/20211013221614-PostCategory.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostsCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'BlogPosts',
+          key: 'id'
+        }
+      },
+
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Categories',
+          key: 'id'
+        }
+      }
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('PostsCategories');
+  }
+};
