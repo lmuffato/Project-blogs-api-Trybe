5200dfdd66708129fb939c72ac4e8abe2dbe17b8
Orlando Flores <orlando_flores@id.uff.br>
2021-10-13 19:56:22 -0300

    feat: cria service para cadastrar usuario

---
+++ services/user.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+const Joi = require('../Joi/templates');
+
+const secret = 'meusegredo';
+
+const jwtConfig = {
+  expiresIn: '7d',
+  algorithm: 'HS256',
+};
+
+const findUserByEmail = async (email) => {
+  const user = await User.findOne((
+    { where: { email } }
+  ));
+  return user;
+};
+
+const createUser = async (body) => {
+  const { error } = Joi.User.validate(body);
+  if (error) return { code: 400, message: error.details[0].message };
+
+  const userAlreadyExists = await findUserByEmail(body.email);
+
+  if (userAlreadyExists) return { code: 409, message: 'User already registered' };
+
+  await User.create(body);
+  const token = jwt.sign({ data: body }, secret, jwtConfig);
+  
+  return token;
+};
+
+module.exports = {
+  createUser,
+};
