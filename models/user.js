e44091bf1d432d9ab88dfbbcdfa490cad77ae4de
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-10-13 20:09:26 -0300

    user model

---
+++ models/user.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      tableName: 'users',
+    },
+  );
+
+  return User;
+};
