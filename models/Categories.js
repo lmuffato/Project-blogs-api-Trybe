9115e9fa98d6da21a6b294c60be19b28f12ec77b
lucas-paz <lucas.hpaz@outlook.com>
2021-10-13 16:11:14 -0300

    creates categories model

---
+++ models/Categories.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'Categories',
+  });
+
+  Category.associate = (models) => {
+    Category.hasMany(models.PostsCategories,
+      { foreignKey: 'categoryId', as: 'PostsCategories' });
+  };
+};
