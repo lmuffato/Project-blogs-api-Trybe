970d0de8df046081141732b6c7757be2bc1ec8f7
ScriptCamilo <script.camilo@gmail.com>
2021-10-13 16:59:44 -0300

    feat: created BlogPost models and migration

---
+++ models/blogpost.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, { 
+    timestamps: false,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'user_id', as: 'users' });
+  };
+
+  return BlogPost;
+};
