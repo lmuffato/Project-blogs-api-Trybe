f062557cffdae7201857da5032518ba25df207ea
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-10-13 16:45:29 -0400

    sequelize init and config.js done

---
+++ config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config({  
+  path: process.env.NODE_ENV === 'test' ? '.env.testing' : '.env',
+});
+
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+};
