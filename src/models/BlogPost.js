8a2adb7de3909d8bd1d13f05bd4e3043d0001e28
rafael <rafittu@outlook.com>
2021-10-13 15:17:09 -0300

    create BlogPosts table and model

---
+++ src/models/BlogPost.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING
+  })
+
+  return BlogPost;
+};
