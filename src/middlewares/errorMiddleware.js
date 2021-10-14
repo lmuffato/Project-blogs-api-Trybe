33e637100378666c1b873409b14d01d8edb4f7a5
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-14 10:43:43 -0300

    refactor: errorMiddleware

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,6 +1,6 @@
 module.exports = (error, _req, res, _next) => {
-  if (error.code && error.status) {
-    return res.status(error.status).json({ message: error.message, code: error.code });
+  if (error.code || error.status) {
+    return res.status(error.status || error.code).json({ message: error.message });
   }
 
   res.status(error.status || 500);
