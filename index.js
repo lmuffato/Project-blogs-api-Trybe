2ef3ae048582d6e8433599859f097f8b565769ff
Joao Nasc <oi@joaonasc.dev>
2021-10-14 17:28:18 -0300

    feat: login route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const userRoutes = require('./routes/user');
+const loginRoutes = require('./routes/login');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 app.use('/user', userRoutes);
+app.use('/login', loginRoutes);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
---
+++ routes/login.js
@@ -0,0 +1,25 @@
+const routes = require('express').Router();
+
+const { User } = require('../models');
+const generateToken = require('../auth/generateToken');
+const schemaValidation = require('../middlewares/schemaValidation');
+
+const loginSchema = require('../schemas/login');
+
+routes.post(
+  '/',
+  (req, res, next) => schemaValidation(req, res, next, loginSchema), 
+  async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const user = await User.findAll({ where: { email, password } });
+      if (!user.length) throw new Error('Invalid fields');
+      const token = generateToken(email);
+      return res.status(200).json({ token });
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  },
+);
+
+module.exports = routes;
