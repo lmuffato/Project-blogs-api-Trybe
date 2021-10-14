493a84420b3a06725e7ee6e24ccecf0281e3c334
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-14 09:14:54 -0300

    fix: add configs

--- src/api/app.js
+++ src/api/app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+
+const routes = require('../routes');
+
+const errorMiddleware = require('../middlewares/errorMiddleware');
+
+const routeNotFound = require('../middlewares/routeNotFound');
+
+app.use(bodyParser.json());
+app.use(cors());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use('/user', routes.UserRoutes);
+app.use('/login', routes.LoginRoutes);
+
+app.use(routeNotFound);
+
+app.use(errorMiddleware);
 
 module.exports = app;
