52fb50ee5b703975122a999e66bb25865644dde7
gabrieldeori <deori.pessoal@gmail.com>
2021-10-14 17:50:43 -0300

    feat: req, res padrao

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
+app.get('/', (_req, res) => {
+  res.send();
 });
