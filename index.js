a7344cfe6c152f890fc97fc708265fdbd917fefe
Carlos Sá <carlos25xrm@gmail.com>
2021-10-14 16:12:57 -0300

    fix: corrigindo index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const { PORT } = process.env || 3000;
+// Se desconstruir não vai pro ou
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
