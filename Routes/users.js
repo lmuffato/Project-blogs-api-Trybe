a1cf61121b2ef5042490d0820a5d4a3882618773
lucas-paz <lucas.hpaz@outlook.com>
2021-10-14 13:45:22 -0300

    route

--- Routes/users.js
+++ Routes/users.js
@@ -2,10 +2,11 @@ const express = require('express');
 const UserController = require('../Controllers/userController');
 
 const { validateUser } = require('../Middlewares/user');
-// const auth = require('../Middlewares/auth');
+const auth = require('../Middlewares/auth');
 
 const router = express.Router();
 
 router.post('/', validateUser, UserController.addNew);
+router.get('/', auth, UserController.listAll);
 
 module.exports = router;
