dc81ca1893b82f1ebbc6d6f76ab203049cdb95e7
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-14 09:16:48 -0300

    feat: create controller LoginController for authentication

---
+++ src/controllers/login/auth.js
@@ -0,0 +1,11 @@
+const LoginService = require('../../services/login');
+
+module.exports = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await LoginService.generateToken(email, password);
+
+  if (!user.token) return res.status(user.status).json({ message: user.message });
+
+  res.status(user.status).json({ token: user.token });
+};
