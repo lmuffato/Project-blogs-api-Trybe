ed134a0ee0544b3b9f2809f4a999d1b83f569d4c
Orlando Flores <orlando_flores@id.uff.br>
2021-10-14 18:12:30 -0300

    feat: cria template Joi para o Post

--- Joi/templates.js
+++ Joi/templates.js
@@ -18,8 +18,15 @@ const Category = Joi.object({
   name: Joi.string().required().not().empty(),
 });
 
+const Post = Joi.object({
+  title: Joi.string().required(),
+  content: Joi.string().required(),
+  categoryIds: Joi.array().required(),
+});
+
 module.exports = {
   User,
   Login,
   Category,
+  Post,
 };
