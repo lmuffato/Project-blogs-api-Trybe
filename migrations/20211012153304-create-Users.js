980dc29d32c4cb40e04f9ee1db92bae52eb2111e
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2021-10-14 15:37:11 -0300

    fix: jwt .env

--- migrations/20211012153304-create-Users.js
+++ migrations/20211012153304-create-Users.js
@@ -16,6 +16,7 @@ module.exports = {
       email: {
         type: Sequelize.STRING,
         allowNull: false,
+        unique: true,
       },
       password: {
         type: Sequelize.STRING,
--- services/UserServices.js
+++ services/UserServices.js
@@ -2,7 +2,7 @@ const { sign } = require('jsonwebtoken');
 const { Users } = require('../models');
 require('dotenv/config');
 
-const { SECRET } = process.env;
+const { JWT_SECRET } = process.env;
 
 const jwtConfig = {
   expiresIn: '3d',
@@ -12,7 +12,7 @@ const jwtConfig = {
 const createUser = async (data) => {
   const newUser = await Users.create(data);
   const { email, image, displayName } = newUser;
-  const token = sign({ data: email, image, displayName }, SECRET, jwtConfig);
+  const token = sign({ data: email, image, displayName }, JWT_SECRET, jwtConfig);
   return { token };
 };
 
