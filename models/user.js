efdcfc54a37ac9baa4ed1bc0beeecf7fc3980af8
mallmann02 <leomallmann020302@gmail.com>
2021-10-14 17:34:46 -0300

    feat: update user model
    Table users are now associated with blog posts

--- models/user.js
+++ models/user.js
@@ -1,13 +1,16 @@
 const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
   },
-  {
-    timestamps: false
-  });
+  { timestamps: false });
+
+  Employee.associate = (models) => {
+    Employee.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'blogPosts' });
+  };
 
   return User;
 };
