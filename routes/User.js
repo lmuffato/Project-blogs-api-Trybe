2e80f9b5905e9c77e32ad83f8f013a6f9a200be4
Orlando Flores <orlando_flores@id.uff.br>
2021-10-14 16:03:05 -0300

    feat: cria rota para /:id

--- routes/User.js
+++ routes/User.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const controller = require('../controllers/user');
 const auth = require('../middlewares/auth');
 
+router.get('/:id', auth, controller.getUserById);
 router.get('/', auth, controller.getAllUsers);
 router.post('/', controller.createUser);
 
