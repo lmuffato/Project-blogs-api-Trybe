7c37dba231352a4ed268875080dacafa5675ac7c
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-13 23:02:58 -0300

    feat: cria rotas base da api

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const usersRoutes = require('./api/routes/users');
+const errors = require('./api/middlewares/errors');
+// const loginRoutes = require('./api/routes/login');
+// const categoriesRoutes = require('./api/routes/categories');
+// const postRoutes = require('./api/routes/post');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use('/user', usersRoutes);
+// app.use('/login', loginRoutes);
+// app.use('/categories', categoriesRoutes);
+// app.use('/post', postRoutes);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
@@ -8,3 +20,7 @@ app.listen(3000, () => console.log('ouvindo porta 3000!'));
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use(errors);
+
+module.exports = app;
