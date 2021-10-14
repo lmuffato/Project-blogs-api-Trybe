24a73be65026c1742f7d6f7c0156881655047309
Jodiel Briesemeister <jodielll29@gmail.com>
2021-10-14 01:29:11 -0300

    feat: check tests

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    // id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
@@ -9,7 +9,6 @@ module.exports = (sequelize, DataTypes) => {
   {
     timestamps: false,
     tableName: 'Users',
-    // underscored: true,
   });
 
   User.associate = (models) => {
