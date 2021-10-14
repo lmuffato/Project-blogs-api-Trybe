2eaf3fc7c21677381d2988d2c1bc261ff66fbdc6
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-14 15:55:52 -0300

    feat: create route for endpoint get /user

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const rescue = require('express-rescue');
 const UserController = require('../controllers/user');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const route = express.Router();
 
 route.post('/', rescue(UserController.createUser));
+route.get('/', authMiddleware, rescue(UserController.findAllUsers));
 
 module.exports = route;
