e074b0a392976d69283a290f7c792c046bad4805
lucas-paz <lucas.hpaz@outlook.com>
2021-10-14 13:09:26 -0300

    creates the route

--- Routes/users.js
+++ Routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const UserController = require('../Controllers/userController');
 
-const { validateUser } = require('../Middlewares/user');
+const { validateUser, validateLogin } = require('../Middlewares/user');
+// const auth = require('../Middlewares/auth');
 
 const router = express.Router();
 
 router.post('/', validateUser, UserController.addNew);
+router.post('/login', validateLogin, UserController.login);
 
 module.exports = router;
