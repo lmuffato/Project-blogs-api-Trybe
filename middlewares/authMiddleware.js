76b72e95740cb524a065afbead1d6de0b2cdf197
Rafael Gerônimo <rafael@geronimo.dev>
2021-10-14 16:31:26 -0300

    fix: remove async function

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,14 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'E$z8TCmXo&Hz6C?Q';
 // const { JWT_SECRET } = process.env;
-const authToken = async (req, res, next) => {
+const authToken = (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
     res.status(401).json({ message: 'Token not found' });
   }
   try {
     const { data } = jwt.verify(token, JWT_SECRET);
-    console.log(data);
     req.user = data;
     return next();
   } catch (_e) {
