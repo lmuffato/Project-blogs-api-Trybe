3d2fb885d3322e659b51c581d25242b7fe1d2c7c
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-14 09:17:06 -0300

    feat: create service LoginService for authentication

---
+++ src/services/login/generateToken.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const { User } = require('../../models');
+
+const { JWT_SECRET } = process.env;
+
+module.exports = async (email, password) => {
+  const userSearch = await User.findOne({ where: { email, password } });
+
+  if (!userSearch) return { status: 401, message: 'invalid fields' };
+
+  const { password: _, ...userPayload } = userSearch;
+
+  const token = jwt.sign(userPayload, JWT_SECRET, { 
+    algorithm: 'HS256',
+    expiresIn: '15d',
+  });
+
+  return { status: 200, token };
+};
