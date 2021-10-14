2724cbd07691e31f364e66c84dfd5ed21c6ddc93
Joao Nasc <oi@joaonasc.dev>
2021-10-14 17:55:37 -0300

    feat: route to list users

--- controllers/user.js
+++ controllers/user.js
@@ -13,4 +13,13 @@ module.exports = {
       return res.status(409).json({ message: 'User already registered' });
     }
   },
+
+  async get(req, res) {
+    try {
+      const users = await User.findAll({ attributes: { exclude: ['password'] } });
+      return res.status(200).json(users);
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  },
 };
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 
+const validateToken = require('../auth/validateToken');
 const userController = require('../controllers/user');
 const schemaValidation = require('../middlewares/schemaValidation');
 const userSchema = require('../schemas/user');
@@ -9,5 +10,7 @@ routes.post(
   (req, res, next) => schemaValidation(req, res, next, userSchema),
   userController.create,
 );
+
+routes.get('/', validateToken, userController.get);
 
 module.exports = routes;
