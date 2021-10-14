5c04379b6ae481198a0444585375d02010ff02a5
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-14 10:42:17 -0300

    refactor: errorMiddleware

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,19 +1,6 @@
-const validateUniqueEmail = (error) => {
-  const [ValidationErrorItem] = error;
-  if (ValidationErrorItem.message === 'Users.email must be unique') {
-    return 'User already registered';
-  }
-  const [message] = error;
-  return message.message;
-};
-
 module.exports = (error, _req, res, _next) => {
   if (error.code && error.status) {
     return res.status(error.status).json({ message: error.message, code: error.code });
-  }
-
-  if (error.errors) {
-    res.status(400).json({ message: validateUniqueEmail(error.errors) });
   }
 
   res.status(error.status || 500);
