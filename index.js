55a5bb847578b8d630f8c1d5ac6b523a5100c2fb
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2021-10-14 14:55:25 -0300

    fix: req01 PORT connection

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParse.json());
 
 app.use('/user', userRouter);
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
