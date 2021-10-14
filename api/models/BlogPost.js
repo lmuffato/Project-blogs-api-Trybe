70a42b072bfcbf483855fa4ae961a15915853393
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-14 17:11:21 -0300

    fix: corrige nome para singular

---
+++ api/models/BlogPost.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+  },
+  {
+    tableName: 'BlogPost',
+    createdAt: 'published',
+    updatedAt: 'updated',
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId', as: 'user',
+    });
+  };
+  return BlogPost;
+};
