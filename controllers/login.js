8e94b0922633a99e813d8222ff82cac31f435d6d
Orlando Flores <orlando_flores@id.uff.br>
2021-10-14 11:20:11 -0300

    feat: cria o controller para o login

---
+++ controllers/login.js
@@ -0,0 +1,13 @@
+const services = require('../services/login');
+
+const login = async (req, res, next) => {
+  const response = await services.login(req.body);
+
+  if (response.code) return next(response);
+
+  return res.status(200).json(response);
+};
+
+module.exports = {
+  login,
+};
