2b9bebd3c643fcf5f0f4920ac676d7507cc1a22a
lucas-paz <lucas.hpaz@outlook.com>
2021-10-14 13:23:08 -0300

    feat: requirement 2

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -8,12 +8,12 @@ module.exports = async (req, _res, next) => {
   const { authorization: token } = req.headers;
   const secret = process.env.JWT_SECRET;
   if (!token) {
-    return next(builtError(401, 'missing auth token'));
+    return next(builtError(400, 'Campos inválidos'));
   }
   try {
     const { data: { email } } = jwt.verify(token, secret);
-    const user = await User.findOne({ where: { title: email } });
-    if (!user) return builtError(401, 'jwt malformed');
+    const user = await User.findOne({ where: { email } });
+    if (!user) return builtError(400, 'Campos inválidos');
     req.user = user;
     next();
   } catch ({ message }) {
