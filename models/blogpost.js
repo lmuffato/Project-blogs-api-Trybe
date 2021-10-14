eaef533abbe572484281e2b479a7f22454121284
mallmann02 <leomallmann020302@gmail.com>
2021-10-14 17:33:31 -0300

    feat: blogpost model

---
+++ models/blogpost.js
@@ -0,0 +1,16 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define("BlogPost", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
+  };
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
