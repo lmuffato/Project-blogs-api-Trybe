eeb849f63069b38ffbedbafe9edb8790359ab78e
lucas-paz <lucas.hpaz@outlook.com>
2021-10-14 13:45:50 -0300

    feat: requirement 3

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -8,15 +8,15 @@ module.exports = async (req, _res, next) => {
   const { authorization: token } = req.headers;
   const secret = process.env.JWT_SECRET;
   if (!token) {
-    return next(builtError(400, 'Campos inválidos'));
+    return next(builtError(401, 'Token not found'));
   }
   try {
     const { data: { email } } = jwt.verify(token, secret);
     const user = await User.findOne({ where: { email } });
-    if (!user) return builtError(400, 'Campos inválidos');
+    if (!user) return builtError(401, 'Expired or invalid token');
     req.user = user;
     next();
   } catch ({ message }) {
-    return next(builtError(401, message));
+    return next(builtError(401, 'Expired or invalid token'));
   }
 };
