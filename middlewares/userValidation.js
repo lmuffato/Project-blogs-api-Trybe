47ecd170907b7e2ce93dce65de73c83e80e3129b
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-10-14 14:44:46 -0400

    fix: :bug: req 02

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -5,7 +5,6 @@ const userValidation = (req, res, next) => {
   const newUser = req.body;
   const { error } = userSchema.validate(newUser);
   if (error) {
-    console.log(error);
     return res.status(STATUS_BAD_REQUEST).json({
       message: error.details[0].message,
     });
