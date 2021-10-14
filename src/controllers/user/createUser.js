e7c05e0d8c3a34422fbe6bc0a02dd0867895b830
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-14 14:57:35 -0300

    refactor: createUser controller

--- src/controllers/user/createUser.js
+++ src/controllers/user/createUser.js
@@ -1,9 +1,22 @@
 const UserServices = require('../../services/user');
+const UserSchema = require('../../schemas/user');
 
 module.exports = async (req, res) => {
   const { displayName, email, password, image } = req.body;
 
+  const userExists = await UserSchema.userEmailExists(email);
+
+  if (userExists.message) {
+    return res
+      .status(userExists.status).json({ message: userExists.message }); 
+  }
+
   const createdUser = await UserServices.createUser({ displayName, email, password, image });
+
+  if (createdUser.message) {
+ return res
+  .status(createdUser.status).json({ message: createdUser.message }); 
+}
 
   res.status(201).json(createdUser);
 };
