4b7c00124bc5097af93e67c9cccf499c59741a7f
gabrieldeori <deori.pessoal@gmail.com>
2021-10-14 17:11:47 -0300

    fix: Transferindo arquivos para pasta src, melhores práticas.

---
+++ src/migrations/20211014183610-create-users.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    const UsersTable = queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        unique: true,
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+    });
+    return UsersTable;
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    queryInterface.dropTable('users');
+  }
+};
