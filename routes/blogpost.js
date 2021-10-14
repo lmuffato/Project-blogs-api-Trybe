1565e8cbf7bca3d849104aa165cd0c5c05228c41
Orlando Flores <orlando_flores@id.uff.br>
2021-10-14 19:22:43 -0300

    feat: adiciona rota get /post

--- routes/blogpost.js
+++ routes/blogpost.js
@@ -3,5 +3,5 @@ const auth = require('../middlewares/auth');
 const controller = require('../controllers/blogpost');
 
 router.post('/', auth, controller.createPost);
-
+router.get('/', auth, controller.getAllPosts);
 module.exports = router;
