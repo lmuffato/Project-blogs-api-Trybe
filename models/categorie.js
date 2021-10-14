dc8b25a6a40fa7cae4b33bcb3e7ad9b44e353d83
mallmann02 <leomallmann020302@gmail.com>
2021-10-14 17:33:40 -0300

    feat: category model

---
+++ models/categorie.js
@@ -0,0 +1,11 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+  },
+  { timestamps: false });
+
+  return Category;
+};
+
+module.exports = Category;
