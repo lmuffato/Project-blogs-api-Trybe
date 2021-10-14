9497082e1fca68695f7ebb1e2bbbff924c9fd053
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-14 14:58:43 -0300

    feat: UserSchema

--- src/schemas/UserSchema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-module.exports = (user) => Joi.object({
-  displayName: Joi.string().min(8),
-  email: Joi.string().email().required(),
-  password: Joi.string().pattern(/.{6}/).required(),
-  image: Joi.string(),
-
-}).validate(user);
