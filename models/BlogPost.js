1b1ce9ab515bbc35f8c590c06e64c895150ab9d9
Jodiel Briesemeister <jodielll29@gmail.com>
2021-10-14 13:28:43 -0300

    feat: check tests[2]

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define('BlogPost', {
-    // id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: { type: DataTypes.STRING, foreignKey: true },
