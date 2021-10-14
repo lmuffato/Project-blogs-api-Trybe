27c6710c09080c7cbd15fb14d4af9f47663a3ea8
Orlando Flores <orlando_flores@id.uff.br>
2021-10-14 15:46:00 -0300

    feat: adiciona a rota /get à rota user

--- routes/User.js
+++ routes/User.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const controller = require('../controllers/user');
+const auth = require('../middlewares/auth');
 
+router.get('/', auth, controller.getAllUsers);
 router.post('/', controller.createUser);
 
 module.exports = router;
