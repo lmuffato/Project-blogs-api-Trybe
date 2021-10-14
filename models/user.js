0cc6236bf73fb14f845b5a22609d0ca7ac75c032
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-10-14 16:15:58 -0400

    model user done

---
+++ models/user.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    image: { type: DataTypes.STRING },
+  },
+  {
+    timestamps: false,
+    tableName: 'Users',
+    underscored: true,
+  });
+
+  return User;
+};
