8f7318ae135ad89b5846e890165fd08fb92ab943
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-14 18:53:01 -0300

    feat: controller CategoryController function createCategory

---
+++ src/controllers/category/createCategory.js
@@ -0,0 +1,24 @@
+const CategoryService = require('../../services/category');
+const CategorySchema = require('../../schemas/categories');
+
+module.exports = async (req, res) => {
+  const { name } = req.body;
+
+  const categoryExists = await CategorySchema.categoryExists(name);
+
+  if (categoryExists.message) {
+    return res
+      .status(categoryExists.status).json({ message: categoryExists.message }); 
+  }
+
+  const createdCategory = await CategoryService.createCategory({ name });
+
+  console.log(createdCategory);
+
+  if (createdCategory.message) {
+    return res
+      .status(createdCategory.status).json({ message: createdCategory.message }); 
+  }
+
+  res.status(201).json(createdCategory);
+};
