2f5ba93cb00ec506fcb98de85c8bc4b605f77cb1
Orlando Flores <orlando_flores@id.uff.br>
2021-10-14 16:40:51 -0300

    feat: cria rota para listar todas as categorias

--- routes/categories.js
+++ routes/categories.js
@@ -3,5 +3,6 @@ const auth = require('../middlewares/auth');
 const controller = require('../controllers/categories');
 
 router.post('/', auth, controller.createCategory);
+router.get('/', auth, controller.getAllCategories);
 
 module.exports = router;
