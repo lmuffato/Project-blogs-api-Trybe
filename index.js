3f5a4717a126c54d0c254863497854caeaa64678
gabrieldeori <deori.pessoal@gmail.com>
2021-10-15 13:18:09 -0300

    feat: adicionando rota

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const controllers = require('./src/controllers');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const { PORT } = process.env;
+
+app.use(bodyParser.json());
+
+// app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_req, res) => {
   res.send();
 });
+
+app.post('/user', controllers.user.register);
