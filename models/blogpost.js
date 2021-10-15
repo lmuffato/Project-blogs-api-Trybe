ddc7c7c407c80a9caafe48374ded1cbcabf60dbd
mallmann02 <leomallmann020302@gmail.com>
2021-10-15 16:05:14 -0300

    update model columns

--- models/blogpost.js
+++ models/blogpost.js
@@ -4,7 +4,9 @@ const BlogPost = (sequelize, DataTypes) => {
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: { type: DataTypes.INTEGER, foreignKey: true },
-  });
+    published: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    updated: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+  }, { timestamps: false });
 
   BlogPost.associate = (models) => {
     BlogPost.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
