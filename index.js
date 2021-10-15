9e1140355c28bd06f0ecaeff8853c8f57d5004e7
mallmann02 <leomallmann020302@gmail.com>
2021-10-14 22:45:16 -0300

    feat: /user endpoint with post method

--- index.js
+++ index.js
@@ -5,9 +5,24 @@ require('dotenv').config();
 const app = express();
 app.use(bodyparser.json());
 
+const UserController = require('./controllers/userController');
+const {
+  validateDisplayName,
+  validatePassword,
+  validateEmail
+} = require('./middlewares/UserCredentials');
+
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.post('/user',
+validateDisplayName,
+validatePassword,
+validateEmail,
+UserController.create);
+
+app.post('/login',);
