b4ed21bade02d96dac6386f944638400f07ce1a7
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-15 16:28:08 -0300

    feat: descomenta rota de categories

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const usersRoutes = require('./api/routes/users');
 const errors = require('./api/middlewares/errors');
 const loginRoutes = require('./api/routes/login');
-// const categoriesRoutes = require('./api/routes/categories');
+const categoriesRoutes = require('./api/routes/categories');
 // const postRoutes = require('./api/routes/post');
 
 const app = express();
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/user', usersRoutes);
 app.use('/login', loginRoutes);
-// app.use('/categories', categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 // app.use('/post', postRoutes);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
