21f65e94969b4ea197d40029172d52e44c1b8f3e
Eduardo Seije <eduardoseije@gmail.com>
2021-10-15 09:51:45 -0300

    Criação do model users e da respectiva migration. Configuração das variáveis de ambiente

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
 
 const app = express();
+app.use(bodyParser.json()); 
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
