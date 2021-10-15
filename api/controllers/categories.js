fc098c94125432c0be7584ca31a0dd4fb59cbfe7
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-15 16:40:26 -0300

    feat: cria funcao getCategories no controller categories

--- api/controllers/categories.js
+++ api/controllers/categories.js
@@ -3,9 +3,13 @@ const categoriesServices = require('../services/categories');
 
 const createCategory = async (req, res, _next) => {
     const { name } = req.body;
-    console.log('chegou auqi');
     const category = await categoriesServices.createCategory(name);
     return res.status(success.created).json(category);
 };
 
-module.exports = { createCategory };
+const getCategories = async (req, res, _next) => {
+    const categories = await categoriesServices.getCategories();
+    return res.status(success.ok).json(categories);
+};
+
+module.exports = { createCategory, getCategories };
