61676cef4cbde139baf4d1fe88aa86382025ff73
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-14 21:16:43 -0300

    feat: cria middleware de validacao do payload de login

--- api/middlewares/users.js
+++ api/middlewares/users.js
@@ -1,9 +1,23 @@
 const userSchema = require('../schemas/users');
+const loginSchema = require('../schemas/login');
 const { clientErrors } = require('../utils/httpStatusCodes');
+const newError = require('../utils/createErrorMessage');
 
 const validateUserPayload = (req, _res, next) => {
     const { displayName, email, password, image } = req.body;
     const { error } = userSchema.validate({ displayName, email, password, image });
+    if (error) {
+        const { message } = error.details[0];
+        const updatedError = newError({ message, statusCode: clientErrors.badRequest });
+
+        return next(updatedError);
+    }
+    return next();
+};
+
+const validateLoginPayload = (req, res, next) => {
+    const { email, password } = req.body;
+    const { error } = loginSchema.validate({ email, password });
     if (error) {
         const { message } = error.details[0];
         const updatedError = { message, statusCode: clientErrors.badRequest };
@@ -12,4 +26,4 @@ const validateUserPayload = (req, _res, next) => {
     return next();
 };
 
-module.exports = { validateUserPayload };
+module.exports = { validateUserPayload, validateLoginPayload };
