a9bc815ae94a6eff997d7912bf09908e00117293
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-10-15 16:10:31 -0300

    carlos-campo-blogs-api

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const app = express();
-
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
