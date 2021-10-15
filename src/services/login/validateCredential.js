634d2128505d78c9ff359b6f0c35453af21e031c
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-15 19:33:55 -0300

    fix: validateCredential

--- src/services/login/validateCredential.js
+++ src/services/login/validateCredential.js
@@ -5,9 +5,9 @@ module.exports = async ({ email, password }) => {
   const { error } = UserSchema.UserValidations({ email, password });
   if (error) return { status: 400, message: error.details[0].message };
 
-  const { dataValues } = await User.findOne({ where: { email, password } }); 
+  const result = await User.findOne({ where: { email, password } }); 
 
-  if (!dataValues) return { status: 400, message: 'Invalid fields' };
+  if (!result) return { status: 400, message: 'Invalid fields' };
   
-  return dataValues;
+  return result.dataValues;
 };
