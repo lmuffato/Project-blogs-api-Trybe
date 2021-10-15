fa614380af2894cd91c2220992fcdf32a9c9899d
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-15 12:00:23 -0300

    feat: adiciona rota que busca todos os usuarios

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,9 +3,11 @@ const rescue = require('express-rescue');
 
 const route = express.Router();
 
-const { createUser } = require('../controllers/users');
+const { createUser, getUsers } = require('../controllers/users');
 const { validateUserPayload } = require('../middlewares/users');
+const validateToken = require('../middlewares/auth');
 
 route.post('/', validateUserPayload, rescue(createUser));
+route.get('/', validateToken, rescue(getUsers));
 
 module.exports = route;
