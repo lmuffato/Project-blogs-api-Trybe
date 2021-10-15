c360ccd4cb9d1098d55d31acbe05b0691c44068a
guilhermemd <guilherme.md@gmail.com>
2021-10-15 18:03:42 -0300

    feat: :sparkles: req 05 fixed up

--- validations/categoryValidation.js
+++ validations/categoryValidation.js
@@ -3,7 +3,7 @@ const { categorySchema } = require('../schema/categorySchema');
 const categoryValidation = (req, res, next) => {
   const categoryName = req.body;
   const { error } = categorySchema.validate(categoryName);
-  if (error) res.status(400).json({ message: error.details[0].message });
+  if (error) next(res.status(400).json({ message: error.details[0].message }));
   
   next();
 };
