02380eac10fb43ed1018576eba2c9063ea1b7709
mallmann02 <leomallmann020302@gmail.com>
2021-10-15 15:53:10 -0300

    fix: missing "await"

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -8,7 +8,7 @@ const create = async (req, res) => {
       return res.status(400).json({ message: 'name is required' });
     }
 
-    const category = Category.create({ name });
+    const category = await Category.create({ name });
     return res.status(201).json(category);
   } catch (e) {
     return res.status(500).json({ message: 'Algo deu errado' });
