70f56059f2715275ded39980bf79c6a1137b3e57
Orlando Flores <orlando_flores@id.uff.br>
2021-10-15 17:43:19 -0300

    feat: adiciona rota para editar post

--- routes/blogpost.js
+++ routes/blogpost.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/blogpost');
 
 router.post('/', auth, controller.createPost);
 router.get('/:id', auth, controller.getPostById);
+router.put('/:id', auth, controller.editPost);
 router.get('/', auth, controller.getAllPosts);
 module.exports = router;
