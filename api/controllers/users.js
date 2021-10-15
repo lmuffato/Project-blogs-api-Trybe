9ce5ff983698dbf6be2e3bfce54087437abb38c8
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-14 21:17:56 -0300

    feat: cria controller de login

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,9 +2,17 @@ const usersService = require('../services/users');
 const { success } = require('../utils/httpStatusCodes');
 
 const createUser = async (req, res, next) => {
-    const user = await usersService.createUser(req.body);
+    const { displayName, email, password, image } = req.body;
+    const user = await usersService.createUser(displayName, email, password, image);
     if (user.message) return next(user);
     return res.status(success.created).json(user);
 };
 
-module.exports = { createUser };
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
+    const token = await usersService.login(email, password);
+    if (token.message) return next(token);
+    return res.status(success.ok).json(token);
+};
+
+module.exports = { createUser, login };
