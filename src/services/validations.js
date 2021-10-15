ae74670f5309a1fad1c1dae93ea0c495e03f8ef2
gabrieldeori <deori.pessoal@gmail.com>
2021-10-15 15:09:15 -0300

    fix: refatorando validations.js

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,17 +1,50 @@
 const { StatusCodes } = require('http-status-codes');
+const { User } = require('../models');
 const { check } = require('../utils');
 
-function user(fields) {
+function invalidateFields(fields) {
   const validation = check.user.validate(fields);
   if (validation.error) {
     const error = validation.error.details[0].message;
     const code = StatusCodes.BAD_REQUEST;
     return { code, error };
   }
+  return false;
+}
+
+async function checkUserExistence(email) {
+  try {
+    const userExists = false;
+    if (userExists) {
+      const error = 'User already registered';
+      const code = StatusCodes.CONFLICT;
+      return { code, error };
+    }
+    return false;
+  } catch (e) {
+    console.log(e);
+    const error = 'Internal server error, se for administrador, cheque os logs';
+    const code = StatusCodes.INTERNAL_SERVER_ERROR;
+    return { code, error };
+  }
+}
+
+function user(fields) {
+  const invalidFields = invalidateFields(fields);
+  if (invalidFields) return invalidFields;
+
+  const userExists = checkUserExistence();
+  if (userExists) return userExists;
+
   const message = 'OK';
   const code = StatusCodes.OK;
   return { code, message };
 }
+
+// const user = services.validations.
+// const user = { exists: 'teste' };
+// if (user.exists) next({ code: user.code, message: user.exists });
+// return res.status(StatusCodes.CONFLICT).json({ message });
 
 module.exports = {
   user,
