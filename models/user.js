70cf939944195101d54b68694adc60c2bfbc2000
Ana Ventura <anaventura1811@gmail.com>
2021-10-14 22:17:24 -0300

    model e migration - user

---
+++ models/user.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User',
+  {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'Users',
+  });
+  return User;
+};
