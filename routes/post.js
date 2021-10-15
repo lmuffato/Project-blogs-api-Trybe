12667b8d72c7daa1094a7d04364976a51901d8d4
Joao Nasc <oi@joaonasc.dev>
2021-10-15 15:08:26 -0300

    fix: bring middleware to check categories back

--- routes/post.js
+++ routes/post.js
@@ -3,11 +3,13 @@ const validateToken = require('../auth/validateToken');
 const postController = require('../controllers/post');
 const postSchema = require('../schemas/post');
 const schemaValidation = require('../middlewares/schemaValidation');
+const categoriesExists = require('../middlewares/categoriesExists');
 
 routes.post(
   '/',
   validateToken,
   (req, res, next) => schemaValidation(req, res, next, postSchema),
+  categoriesExists,
   postController.create,
 );
 
