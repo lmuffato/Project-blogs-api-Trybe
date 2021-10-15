dddfab9a0503485c1c1463cfa215952938153997
mallmann02 <leomallmann020302@gmail.com>
2021-10-15 15:37:42 -0300

    feat: user token authentication

---
+++ auth/validateJWT.js
@@ -0,0 +1,49 @@
+// validateJWT.js
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+/* Mesma chave privada que usamos para criptografar o token.
+   Agora, vamos usá-la para descriptografá-lo.
+   Numa aplicação real, essa chave jamais ficaria hardcoded no código assim,
+   e muitos menos de forma duplicada, mas aqui só estamos interessados em
+   ilustrar seu uso ;) */
+const secret = 'secret123';
+
+module.exports = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secret);
+
+    /*
+      A variável decoded será um objeto equivalente ao seguinte:
+      {
+        data: {
+          _id: '5e54590ba49448f7e5fa73c0',
+          username: 'italssodj',
+          password: 'senha123'
+        },
+        iat: 1582587327,
+        exp: 1584774714908
+      }
+    */
+
+    const user = await User.findOne({ where: { email: decoded.data.email } });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: 'Expired or invalid token' });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
