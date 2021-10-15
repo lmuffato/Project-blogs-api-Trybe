5a19124b89825616df5bbf11db93ace6bfeb750a
mallmann02 <leomallmann020302@gmail.com>
2021-10-15 18:15:08 -0300

    fix: error message

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,7 +5,7 @@ const create = async (req, res) => {
     const { name } = req.body;
 
     if (!name) {
-      return res.status(400).json({ message: 'name is required' });
+      return res.status(400).json({ message: '\"name\" is required' });
     }
 
     const category = await Category.create({ name });
