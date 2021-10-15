2bfad2378f2c22bb3ee132934be27386558fd959
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-10-15 13:25:49 -0300

    Feat: cria model PostsCategory

---
+++ models/PostCategory.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, _DataTypes) => {
+  const PostsCategory = sequelize.define('PostsCategory', {}, { timestamps: false });
+  PostsCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostsCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostsCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+  return PostsCategory;
+};
