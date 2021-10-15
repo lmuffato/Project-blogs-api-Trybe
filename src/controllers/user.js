4bf72833addae27da51c9feff6353e90829c8e6e
gabrieldeori <deori.pessoal@gmail.com>
2021-10-15 14:37:20 -0300

    feat: adicionando controlador de usuario

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,14 +1,13 @@
-const {
-  StatusCodes: {
-    CREATED,
-  },
-} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
+const services = require('../services');
 
-async function register(req, res) {
-  const fields = req.body;
-  
-  const token = 'devolverToken';
-  return res.status(CREATED).json({ token });
+async function register(req, res, next) {
+  const userFields = req.body;
+  const validateUser = services.validations.user(userFields);
+  if (validateUser.error) next({ code: validateUser.code, message: validateUser.error });
+
+  const token = '123';
+  return res.status(StatusCodes.CREATED).json({ token });
 }
 
 module.exports = {
