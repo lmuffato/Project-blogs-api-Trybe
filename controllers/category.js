bf6839ad816a7b88d9520b562fadced4b543f9e1
Ana Ventura <anaventura1811@gmail.com>
2021-10-15 04:52:11 -0300

    agora vai

--- controllers/category.js
+++ controllers/category.js
@@ -1,8 +1,6 @@
 const { Category } = require('../models');
 const httpStatus = require('../utils/httpStatus');
 
-// const errorCodes = require('../utils/errorCodes');
-
 const create = async (req, res) => {
   const { name } = req.body;
 
