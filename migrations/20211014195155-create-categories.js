3d8bfc56f67cc9ff64638994a3db526d0903df8c
mallmann02 <leomallmann020302@gmail.com>
2021-10-15 18:43:28 -0300

    refactor: set timestamp option to false

--- migrations/20211014195155-create-categories.js
+++ migrations/20211014195155-create-categories.js
@@ -11,8 +11,7 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
-    },
-    { timestamps: false });
+    });
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Categories');
