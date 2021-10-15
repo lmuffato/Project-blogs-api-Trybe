9814b119d74a7f64132419293d216cbb2f84b931
gabrieldeori <deori.pessoal@gmail.com>
2021-10-15 11:42:36 -0300

    fix: Refatorando sequelize

--- src/models/category.js
+++
@@ -1,6 +0,0 @@
-module.exports = (sequelize, DataTypes) => sequelize.define('Category', {
-  name: DataTypes.STRING,
-}, {
-  tableName: 'Categories',
-  timestamps: false,
-});
--- src/models/user.js
+++
@@ -1,6 +0,0 @@
-module.exports = (sequelize, DataTypes) => sequelize.define('User', {
-  displayName: DataTypes.STRING,
-  email: DataTypes.STRING,
-  password: DataTypes.STRING,
-  image: DataTypes.STRING,
-}, { timestamps: false });
