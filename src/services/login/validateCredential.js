02254df1e954deb409f2c1d7ef6cce391d63552d
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-15 10:56:36 -0300

    fix: data use in variable dataValues - { dataValues }

--- src/services/login/validateCredential.js
+++ src/services/login/validateCredential.js
@@ -5,9 +5,9 @@ module.exports = async ({ email, password }) => {
   const { error } = UserSchema.UserValidations({ email, password });
   if (error) return { status: 400, message: error.details[0].message };
 
-  const userSearch = await User.findOne({ where: { email, password } });  
+  const { dataValues } = await User.findOne({ where: { email, password } }); 
 
-  if (!userSearch) return { status: 400, message: 'Invalid fields' };
+  if (!dataValues) return { status: 400, message: 'Invalid fields' };
   
-  return userSearch;
+  return dataValues;
 };
