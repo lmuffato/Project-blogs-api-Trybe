5d1a6a1cd3a31fa4e894e6ee2f96fbf62c5f55f9
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-15 16:39:51 -0300

    feat: cria rota  ara egar todas as categorias

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const validateToken = require('../middlewares/auth');
-const { createCategory } = require('../controllers/categories');
+const { createCategory, getCategories } = require('../controllers/categories');
 const validateCategoryPayload = require('../middlewares/categories');
 
 const route = express.Router();
 
 route.post('/', validateCategoryPayload, validateToken, createCategory);
+route.get('/', validateToken, getCategories);
 
 module.exports = route;
