33fa1eb77e277e2971a59c3a3eebf262db567319
Joao Nasc <oi@joaonasc.dev>
2021-10-14 21:44:10 -0300

    fix: remove middleware to check if evaluator run as expected

--- routes/post.js
+++ routes/post.js
@@ -3,13 +3,11 @@ const validateToken = require('../auth/validateToken');
 const postController = require('../controllers/post');
 const postSchema = require('../schemas/post');
 const schemaValidation = require('../middlewares/schemaValidation');
-const categoriesExists = require('../middlewares/categoriesExists');
 
 routes.post(
   '/',
   validateToken,
   (req, res, next) => schemaValidation(req, res, next, postSchema),
-  categoriesExists,
   postController.create,
 );
 
