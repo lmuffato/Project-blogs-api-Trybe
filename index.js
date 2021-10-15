8be6f8e56a807afde6941c65555e2ada01361ac8
mallmann02 <leomallmann020302@gmail.com>
2021-10-15 15:37:17 -0300

    feat: define user and categories endpoints

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const app = express();
 app.use(bodyparser.json());
 
 const UserController = require('./controllers/userController');
+const CategoryController = require('./controllers/categoryController');
 const login = require('./controllers/login');
 const {
   validateDisplayName,
   validatePassword,
   validateEmail
 } = require('./middlewares/UserCredentials');
+const validateJWT = require('./auth/validateJWT');
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
@@ -25,5 +27,21 @@ validateDisplayName,
 validatePassword,
 validateEmail,
 UserController.create);
+
+app.get('/user',
+validateJWT,
+UserController.getAll);
+
+app.get('/user/:id',
+validateJWT,
+UserController.findUser);
+
+app.post('/categories',
+validateJWT,
+CategoryController.create);
+
+app.get('/categories',
+validateJWT,
+CategoryController.getAll);
 
 app.post('/login', login);
