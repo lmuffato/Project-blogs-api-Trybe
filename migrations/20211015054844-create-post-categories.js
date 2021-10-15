799545b8ffdad4cd85738205b650c62223090686
Ana Ventura <anaventura1811@gmail.com>
2021-10-15 05:22:57 -0300

    fix: corrige typo na migration de post categories

--- migrations/20211015054844-create-post-categories.js
+++ migrations/20211015054844-create-post-categories.js
@@ -1,31 +1,31 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('PostCategories', {
+    await queryInterface.createTable('PostsCategories', {
 			postId: {
 				type: Sequelize.INTEGER,
-				allowNull: false,
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-        references: {
-          model: 'BlogPosts',
-          key: 'id',
-        }
+				// allowNull: false,
+        // onUpdate: 'CASCADE',
+        // onDelete: 'CASCADE',
+        // references: {
+        //   model: 'BlogPosts',
+        //   key: 'id',
+        // }
 			},
       categoryId: {
         type: Sequelize.INTEGER,
-        allowNull: false,
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-        references: {
-          model: 'Categories',
-          key: 'id',
-        },
+        // allowNull: false,
+        // onUpdate: 'CASCADE',
+        // onDelete: 'CASCADE',
+        // references: {
+        //   model: 'Categories',
+        //   key: 'id',
+        // },
       }
 		});
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('PostCategories');
+    await queryInterface.dropTable('PostsCategories');
   }
 };
 
