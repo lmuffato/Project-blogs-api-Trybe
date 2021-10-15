1bcdb254ae4005ffc0287736aeab9337d45dedc0
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-15 19:10:02 -0300

    feat: add route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const UserRoutes = require('./UserRoutes');
 const LoginRoutes = require('./LoginRoutes');
 const CategoryRoutes = require('./CategoryRoutes');
+const BlogPostRoutes = require('./BlogPostRoutes');
 
 module.exports = {
   UserRoutes,
   LoginRoutes,
   CategoryRoutes,
+  BlogPostRoutes,
 };
