f0aad5d91ec3a03cfafd57c1f763e1cec9aea4e5
Orlando Flores <orlando_flores@id.uff.br>
2021-10-15 17:43:08 -0300

    feat: cria controller para editar o post

--- controllers/blogpost.js
+++ controllers/blogpost.js
@@ -22,8 +22,23 @@ const getPostById = async (req, res, next) => {
   return res.status(200).json(response);
 };
 
+const editPost = async (req, res, next) => {
+  const { id: userId } = req.user;
+  const { id } = req.params;
+  const { title, content } = req.body;
+
+  if (req.categoryIds) return res.status(400).json({ message: 'Categories cannot be edited' });
+
+  const response = await services.editPost({ id, title, content }, userId);
+
+  if (response.code) return next(response);
+
+  return response;
+};
+
 module.exports = {
   createPost,
   getAllPosts,
   getPostById,
+  editPost,
 };
