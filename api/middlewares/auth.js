df1e5f956452249c7715248962193a8f557fefdd
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-15 11:55:56 -0300

    feat: cria middleware de autenticaçao atraves de jwt

---
+++ api/middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const { clientErrors } = require('../utils/httpStatusCodes');
+const { getUserById } = require('../services/users');
+const newError = require('../utils/createErrorMessage');
+
+const secret = process.env.JWT_SECRET;
+const { unauthorized } = clientErrors;
+
+const tokenNotFound = newError('Token not found', unauthorized);
+const expiredOrInvalidToken = newError('Expired or invalid token', unauthorized);
+const validateToken = async (req, _res, next) => {
+    const { authorization: token } = req.headers;
+
+    if (!token) return next(tokenNotFound);
+
+    try {
+        const decoded = jwt.verify(token, secret);
+        const { id } = decoded;
+
+        const user = await getUserById(id);
+
+        if (!user) return next(expiredOrInvalidToken);
+
+        return next();
+    } catch (error) {
+        return next(expiredOrInvalidToken);
+    }
+};
+
+module.exports = validateToken;
