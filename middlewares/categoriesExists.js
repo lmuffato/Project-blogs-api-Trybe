87257a01655bd4cf2acee8343c424ec4d2dd93db
Joao Nasc <oi@joaonasc.dev>
2021-10-14 21:15:28 -0300

    feat: middleware to check if all categories exists

---
+++ middlewares/categoriesExists.js
@@ -0,0 +1,11 @@
+const { Category } = require('../models');
+
+module.exports = (req, res, next) => {
+  const { categoryIds } = req.body;
+  categoryIds.forEach(async (id) => {
+    if (await Category.findByPk(id) === null) {
+      return res.status(400).json({ message: '"categoryIds" not found' });
+    }
+  });
+  next();
+};
