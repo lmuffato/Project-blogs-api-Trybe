26f8d4354655cfa8ba1acb0e86859c849a0db6a1
ScriptCamilo <script.camilo@gmail.com>
2021-10-15 10:56:30 -0300

    feat: verify token validation

--- src/validations/token/index.js
+++ src/validations/token/index.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const { getStatusCode } = require('../../utils/statusCode');
+const { throwError } = require('../../utils/error');
 
 const { JWT_SECRET } = process.env;
 
@@ -12,14 +14,24 @@ function getToken(email) {
   return token;
 }
 
-// function verifyToken(token) {
-//   try {
-    
-//   } catch (error) {
-    
-//   }
-// }
+function verifyToken(token) {
+  try {
+    if (!token) {
+      const error = new Error('Token not found');
+      error.nullToken = true;
+      throw error;
+    }
+
+    const user = jwt.verify(token, JWT_SECRET);
+
+    return user;
+  } catch ({ nullToken, message }) {
+    const errorMessage = nullToken ? message : 'Expired or invalid token';
+    throwError('unauthorized', errorMessage, getStatusCode);
+  }
+}
 
 module.exports = {
   getToken,
+  verifyToken,
 };
