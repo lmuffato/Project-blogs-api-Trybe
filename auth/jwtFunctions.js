e81bf39c37eaee4d6bfed73dac5eb8910caa0633
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-10-15 19:04:46 -0300

    Feat: create auth/jwtFunctions.js

---
+++ auth/jwtFunctions.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const SECRET = process.env.JWT_SECRET;
+
+const create = (user) => {
+  const { password, ...payload } = user;
+  const token = jwt.sign(payload, SECRET);
+  return token;
+};
+
+const verify = (token) => {
+  const payload = jwt.verify(token, SECRET);
+  return payload;
+};
+
+module.exports = { create, verify };
