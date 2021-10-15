fdd435231704b9928a2ebd4dd15e21b09a23c591
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-14 21:18:44 -0300

    feat: cria serviço de login para geraçao de token jwt

--- api/services/users.js
+++ api/services/users.js
@@ -1,19 +1,32 @@
+const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 const { clientErrors } = require('../utils/httpStatusCodes');
 const newError = require('../utils/createErrorMessage');
 
-const existUser = async (email) => {
-    const user = await User.findOne({ where: { email } });
-    return user;
-};
+const secret = process.env.JWT_SECRET;
+const jwtConfig = { expiresIn: '12h', algorithm: 'HS256' };
 
-const createUser = async (userData) => {
-    const { displayName, email, password, image } = userData;
+const existUser = async (email) => User.findOne({ where: { email } });
+
+const createUser = async (displayName, email, password, image) => {
     const existUserEmail = await existUser(email);
     if (existUserEmail) return newError('User already registered', clientErrors.conflict);
-
     const createdUser = await User.create({ displayName, email, password, image });
     return createdUser;
 };
 
-module.exports = { existUser, createUser };
+const existLoginData = async (email, password) => 
+ User.findOne({ where: { email, password } });
+
+const login = async (email, password) => {
+    const user = await existLoginData(email, password);
+    if (user) {
+        const { id, displayName, image } = user;
+        const userData = { id, displayName, email, image };
+        const token = jwt.sign(userData, secret, jwtConfig);
+        return { token };
+    }
+    return newError('Invalid fields', clientErrors.badRequest);
+};
+
+module.exports = { createUser, login };
