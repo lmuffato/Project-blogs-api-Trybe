7e5f75075291739d57787765f10e648f61a40296
Rodrigo Rech <rodrigorech92@gmail.com>
2021-10-15 15:25:51 -0300

    Fix: json messages

--- middlewares/validateUsers.js
+++ middlewares/validateUsers.js
@@ -2,9 +2,9 @@ const database = require('../models');
 
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
-  if (!email) return res.status(400).json({ message: 'email is required' });
+  if (!email) return res.status(400).json({ message: '"email" is required' });
   if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    .test(email)) return res.status(400).json({ message: 'email must be a valid email' });
+    .test(email)) return res.status(400).json({ message: '"email" must be a valid email' });
   const doUserExists = database.User.findOne({ where: { email } });
   console.log(doUserExists);
   if (doUserExists) return res.status(409).json({ message: 'User already registered' });
@@ -13,19 +13,19 @@ const validateEmail = (req, res, next) => {
 
 const validateName = (req, res, next) => {
   const { displayName } = req.body;
-  if (!displayName) return res.status(400).json({ message: 'displayName is required' });
+  if (!displayName) return res.status(400).json({ message: '"displayName" is required' });
   if (displayName.length < 8) { 
     return res.status(400)
-    .json({ message: 'displayName length must be at least 8 characters long' });
+    .json({ message: '"displayName" length must be at least 8 characters long' });
   }
   next();
 };
 
 const validatePassword = (req, res, next) => {
   const { password } = req.body;
-  if (!password) return res.status(400).json({ message: 'password is required' });
+  if (!password) return res.status(400).json({ message: '"password" is required' });
   if (password.length < 6) {
-    return res.status(400).json({ message: 'password length must be 6 characters long' });
+    return res.status(400).json({ message: '"password" length must be 6 characters long' });
   }
   next();
 };
