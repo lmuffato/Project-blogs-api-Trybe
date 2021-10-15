0ac0a52d345072a15e42ff7d9f02be617a1258d6
Maria Baeta <m.martinsdg@gmail.com>
2021-10-15 09:36:49 -0300

    feat: :bug: Tentando corrigir

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const userController = require('./controllers/userController');
 const loginController = require('./controllers/loginController');
 const categoriesController = require('./controllers/categoriesController');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.json());
-// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded(({ extended: true })));
+app.use(bodyParser.json());
 
 app.use('/user', userController);
 app.use('/login', loginController);
 app.use('/categories', categoriesController);
-app.use('/', (req, _res) => {
-  console.log('Aqui', req.body);
-});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}`));
-// module.exports = app;
