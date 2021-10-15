3d61a43af04d6e14ab5f9791d801cc2b2ade4d7c
Eduardo Seije <eduardoseije@gmail.com>
2021-10-15 12:20:48 -0300

    Criação do tokenValidation

---
+++ auth/tokenValidation.js
@@ -0,0 +1,29 @@
+// feito com base no mesmo validador do projeto cookmaster.
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const {
+  ERROR_TOKEN,
+  ERROR_JWT,
+} = require('../utils/errors');
+
+const SECRET = process.env.SECRET || 'meusupersegredo';
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(ERROR_TOKEN.error.status).json({ message: ERROR_TOKEN.error.message });
+  }
+  try {
+    const payload = jwt.verify(token, SECRET);
+    // console.log(payload);
+    req.payload = payload;
+    return next();
+  } catch (error) {
+    res.status(ERROR_JWT.error.status).json({ message: ERROR_JWT.error.message });
+  }
+};
+
+module.exports = {
+  authMiddleware,
+};
