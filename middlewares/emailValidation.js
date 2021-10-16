04422c74e4165fc452c339005728927d94a0af13
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-10-15 21:04:21 -0300

    adding empty email case

--- middlewares/emailValidation.js
+++ middlewares/emailValidation.js
@@ -1,15 +1,21 @@
+const BAD_REQUEST = 400;
+
 function validateEmail(req, res, next) {
   const { email } = req.body;
+  
+  if (email === undefined) {
+    return res.status(BAD_REQUEST).json({ message: '"email" is required' });
+  }
 
-  if (!email) {
-    return res.status(400).json({ message: '"email" is required' });
+  if (!email.length) {
+    return res.status(BAD_REQUEST).json({ message: '"email" is not allowed to be empty' });
   }
 
   const emailRegex = /[a-z0-9]+@[a-z0-9]+(\.com)$/gi;
   const emailIsValid = emailRegex.test(email);
 
   if (!emailIsValid) {
-    return res.status(400).json({ message: '"email" must be a valid email' });
+    return res.status(BAD_REQUEST).json({ message: '"email" must be a valid email' });
   }
   
   next();
