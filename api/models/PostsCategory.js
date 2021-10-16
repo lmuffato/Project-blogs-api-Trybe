7fac7aed2bc6ca63bb96ba874622adca3f405f10
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-16 19:58:44 -0300

    fix: adiciona aliases que estavam faltando nas associations

--- api/models/PostsCategory.js
+++ api/models/PostsCategory.js
@@ -1,15 +1,17 @@
 module.exports = (sequelize, _DataTypes) => {
     const PostsCategory = sequelize.define('PostsCategory', {},
-        { timestamps: false, tableName: 'PostsCategory' });
+        { timestamps: false });
 
         PostsCategory.associate = (models) => {
         models.Category.belongsToMany(models.BlogPost, {
             through: PostsCategory,
+            as: 'posts',
             foreignKey: 'categoryId',
             otherKey: 'postId',
         });
         models.BlogPost.belongsToMany(models.Category, {
             through: PostsCategory,
+            as: 'categories',
             foreignKey: 'postId',
             otherKey: 'categoryId',
         });
