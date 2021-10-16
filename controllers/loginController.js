62c634cbede54ecaf0a23c8d5204e7a0bf9acba6
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-10-15 21:03:46 -0300

    login controller

---
+++ controllers/loginController.js
@@ -0,0 +1,17 @@
+const loginService = require('../services/loginService');
+
+async function login(req, res) {
+  const { email, password } = req.body;
+
+  const { code, message, token } = await loginService.login({ email, password });
+
+  if (message) {
+    return res.status(code).json({ message });
+  }
+
+  res.status(code).json({ token });
+}
+
+module.exports = {
+  login,
+};
