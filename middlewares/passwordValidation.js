24bd2e067abf8a235e6f9fc1da75af73f84cd9aa
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-10-15 21:04:32 -0300

    adding empty password case

--- middlewares/passwordValidation.js
+++ middlewares/passwordValidation.js
@@ -1,14 +1,20 @@
+const BAD_REQUEST = 400;
+
 function validatePassword(req, res, next) {
   const { password } = req.body;
 
-  if (!password) {
-    return res.status(400).json({ message: '"password" is required' });
+  if (password === undefined) {
+    return res.status(BAD_REQUEST).json({ message: '"password" is required' });
+  }
+
+  if (!password.length) {
+    return res.status(BAD_REQUEST).json({ message: '"password" is not allowed to be empty' });
   }
 
   const passwordIsValid = password.length === 6;
 
   if (!passwordIsValid) {
-    return res.status(400).json({ message: '"password" length must be 6 characters long' });
+    return res.status(BAD_REQUEST).json({ message: '"password" length must be 6 characters long' });
   }
   
   next();
