41b4d9e0088db1d5bd77b3671aff71445b695920
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-10-15 22:25:19 -0300

    new getAll controller

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
+
+const { validateJWT } = require('../middlewares/authMiddleware');
 const { validateEmail } = require('../middlewares/emailValidation');
 const { validateName } = require('../middlewares/nameValidation');
 const { validatePassword } = require('../middlewares/passwordValidation');
-const { addUser } = require('../controllers/userController');
+
+const userController = require('../controllers/userController');
 
 const router = Router();
 
@@ -11,7 +14,13 @@ router.post(
   validateEmail,
   validateName,
   validatePassword,
-  addUser,
+  userController.addUser,
+);
+
+router.get(
+  '/',
+  validateJWT,
+  userController.getAll,
 );
 
 module.exports = router;
