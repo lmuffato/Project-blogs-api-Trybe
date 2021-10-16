82bb8ad2e5ffd465fdd959db71ead4af879e4b11
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-10-15 21:04:46 -0300

    login service

---
+++ services/loginService.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+const { User } = require('../models');
+require('dotenv').config();
+
+const JWTconfig = {
+  expiresIn: '7d',
+  algorithm: 'HS256',
+};
+
+async function login({ email, password }) {
+  const userByEmail = await User.findOne(
+    { where: { email } },
+  );
+  console.log(userByEmail);
+  if (!userByEmail) {
+    return { code: 400, message: 'Invalid fields' };
+  }
+
+  const JWTpayload = { email };
+
+  const token = jwt.sign(JWTpayload, process.env.JWT_SECRET, JWTconfig);
+
+  return { code: 200, token };
+}
+
+module.exports = {
+  login,
+};
