1afb6c5034d4050b0d2d8fdc2b93a9e4ac9b41a7
luanlsr <luanfswd@gmail.com>
2021-10-16 15:24:20 -0300

    feat: :sparkles: req 8 implemented

--- services/postService.js
+++ services/postService.js
@@ -1,5 +1,5 @@
 // const { StatusCodes: { OK, BAD_REQUEST } } = require('http-status-codes');
-const { BlogPost } = require('../models');
+const { BlogPost, User, Category } = require('../models');
 
 const createPost = async ({ title, content, categoryIds, userId }) => {
   const result = await BlogPost.create({ 
@@ -11,6 +11,18 @@ const createPost = async ({ title, content, categoryIds, userId }) => {
   return result;
 };
 
+const findAll = async () => {
+  const result = await BlogPost.findAll({
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+      // este trecho fiz com base no código do Adelino
+    ],
+  });
+  return result;
+};
+
 module.exports = {
   createPost,
+  findAll,
 };
