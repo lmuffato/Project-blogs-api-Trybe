3b525a3ab48f9ac340b79c4f689ce0aa22f2fdef
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-16 14:49:21 -0300

    feat: create endpoint put /post/:id

--- src/routes/BlogPostRoutes.js
+++ src/routes/BlogPostRoutes.js
@@ -8,5 +8,6 @@ const route = express.Router();
 route.post('/', authMiddleware, rescue(BlogPostControllers.createPost));
 route.get('/', authMiddleware, rescue(BlogPostControllers.findAllPosts));
 route.get('/:id', authMiddleware, rescue(BlogPostControllers.findByPkPost));
+route.put('/:id', authMiddleware, rescue(BlogPostControllers.updatePost));
 
 module.exports = route;
