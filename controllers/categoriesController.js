3ca2666d727c7a4273b69596e1104bf8811c2ff9
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-10-17 19:14:50 -0300

    moving category name validation to a separate middeware

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -3,10 +3,6 @@ const categoriesService = require('../services/categoriesService');
 async function create(req, res) {
   const { name } = req.body;
 
-  if (!name) {
-    return res.status(400).json({ message: '"name" is required' });
-  }
-
   const { code, created } = await categoriesService.create(name);
 
   res.status(code).json(created);
--- routers/categoriesRouter.js
+++ routers/categoriesRouter.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 
 const { validateJWT } = require('../middlewares/authMiddleware');
+
+const { validateCategory } = require('../middlewares/categoryValidation');
 const categoriesController = require('../controllers/categoriesController');
 
 const router = Router();
 
 router.post(
   '/',
+  validateCategory,
   validateJWT,
   categoriesController.create,
 );
