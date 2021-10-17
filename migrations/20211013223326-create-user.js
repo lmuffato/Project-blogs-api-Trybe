ecfb68c6313099e4a0dea49f0d6371efbb284b1d
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-10-17 17:04:31 -0300

    removnig createdAt e updatedAt from migration

--- migrations/20211013223326-create-user.js
+++ migrations/20211013223326-create-user.js
@@ -19,14 +19,6 @@ module.exports = {
       },
       image: {
         type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
