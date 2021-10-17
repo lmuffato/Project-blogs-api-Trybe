2912beba5107155c01ae3e50ffdafe9d0bfa8353
Orlando Flores <orlando_flores@id.uff.br>
2021-10-17 14:08:14 -0300

    feat: cria rota para deletar post por id

--- routes/blogpost.js
+++ routes/blogpost.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/blogpost');
 router.post('/', auth, controller.createPost);
 router.get('/:id', auth, controller.getPostById);
 router.put('/:id', auth, controller.editPost);
+router.delete('/:id', auth, controller.deletePostById);
 router.get('/', auth, controller.getAllPosts);
 module.exports = router;
