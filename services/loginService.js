ed61851e14f546c02b6a9c0ace4fbc8417b925ec
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-10-17 13:45:37 -0300

    remove: console.log

--- services/loginService.js
+++ services/loginService.js
@@ -2,7 +2,7 @@ const { User } = require('../models');
 
 const findLogin = async ({ email }) => {
   const findEmail = await User.findOne({ where: { email } });
-  console.log('🚀 ~ file: loginService.js ~ line 5 ~ findLogin ~ findEmail', findEmail);
+  // console.log('🚀 ~ file: loginService.js ~ line 5 ~ findLogin ~ findEmail', findEmail);
   if (!findEmail) {
     return { error: true, message: 'Invalid fields', status: 400 };
   }
