698dd585db8f1a40c61180337f7dc6ef902504bb
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-18 17:44:36 -0300

    fix: a error in the line of associate, because the model names isnt correct

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   User.associate = (models) => {
-    User.hasMany(models.User,
+    User.hasMany(models.BlogPost,
      { foreignKey: 'user_id', as: 'blog_posts' });
   };
 
