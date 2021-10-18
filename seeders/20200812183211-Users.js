3c308cf503a5810f9a7e5f35b865793d3c8586a4
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-18 16:34:26 -0300

    fix: change the column name in the seeders to maintain default rules about comun name in the MySQL

--- seeders/20200812183211-Users.js
+++ seeders/20200812183211-Users.js
@@ -3,14 +3,14 @@ module.exports = {
     await queryInterface.bulkInsert('Users',
       [{
         id: 1,
-        displayName: 'Lewis Hamilton',
+        display_name: 'Lewis Hamilton',
         email: 'lewishamilton@gmail.com',
         password: '123456',
         image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg',
       },
       {
         id: 2,
-        displayName: 'Michael Schumacher',
+        display_name: 'Michael Schumacher',
         email: 'MichaelSchumacher@gmail.com',
         password: '123456',
         image: 'https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg',
