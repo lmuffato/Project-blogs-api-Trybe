4bdda33195c6f53dd8304f85bc97a4b4abfd4431
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-18 18:49:55 -0300

    Created the login controller for User

--- controller/userController.js
+++ controller/userController.js
@@ -8,6 +8,15 @@ const create = async (req, res) => {
   return res.status(status).json(data);
 };
 
+const login = async (req, res) => {
+  const { status, data, message } = await Users.login(req.body);
+
+  if (message) return res.status(status).json({ message });
+
+  return res.status(status).json(data);
+};
+
 module.exports = {
   create,
+  login,
 };
