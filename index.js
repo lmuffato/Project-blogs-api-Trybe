efdfa931bb8df804557c4fc663927751c63b4325
WillianMendes <willianmendesbr@gmail.com>
2021-10-19 20:44:27 -0300

    fix: reset project

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const UserRoutes = require('./routes/UserRoute');
 
 const app = express();
-app.use(express.json());
-dotenv.config();
 
-const PORT = process.env.PORT || 3000;
-
-app.use(UserRoutes);
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
-
-app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
