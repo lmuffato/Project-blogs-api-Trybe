d8717b731e7ea6089dc36770b9db7b8fe42e5acb
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-19 13:03:22 -0300

    Created a controller getById for User

--- controller/userController.js
+++ controller/userController.js
@@ -16,8 +16,18 @@ const login = async (req, res) => {
   return res.status(status).json(data);
 };
 
-const getAll = async (req, res) => {
+const getAll = async (_req, res) => {
   const { status, data, message } = await Users.getAll();
+
+  if (message) return res.status(status).json({ message });
+
+  return res.status(status).json(data);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  
+  const { status, data, message } = await Users.getById(id);
 
   if (message) return res.status(status).json({ message });
 
@@ -28,4 +38,5 @@ module.exports = {
   create,
   login,
   getAll,
+  getById,
 };
