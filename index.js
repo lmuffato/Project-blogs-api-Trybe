e921fd2bf018afcd8d137fa9be992c0c4314df76
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-10-18 21:28:14 -0300

    getUserById working but not passing on tests

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(bodyParser.json());
 
 app.post('/user', userController.createUser);
 app.post('/login', userController.loginUser);
+app.get('/user/:id', validateToken, userController.getUserById);
 app.get('/user', validateToken, userController.getUser);
-// app.get('/user', userController.getUser);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
