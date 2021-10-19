a12f6c2ade8fb2093f5cb3bbba945641e9428425
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-19 13:22:52 -0300

    Created a controller for Category and a create function there

---
+++ controller/categoryController.js
@@ -0,0 +1,13 @@
+const Category = require('../services/categoryService');
+
+const create = async (req, res) => {
+  const { status, data, message } = await Category.create(req.body);
+
+  if (message) return res.status(status).json({ message });
+
+  return res.status(status).json(data);
+};
+
+module.exports = {
+  create,
+};
