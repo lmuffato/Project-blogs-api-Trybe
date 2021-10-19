4c7da3d86ca6989a930ac8bd0b59216f7a7c7477
WillianMendes <willianmendesbr@gmail.com>
2021-10-19 20:10:04 -0300

    fix: connection refused

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ app.use(express.json());
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
+
+app.use(UserRoutes);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
-
-app.use(UserRoutes);
-
-app.listen(PORT, '127.0.0.1', () => console.log(`ouvindo porta ${PORT}!`));
