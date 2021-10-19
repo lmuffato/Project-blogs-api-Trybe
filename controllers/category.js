305866c2fb663e9175f0c339e9e9b9fbecb8eca8
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-10-19 13:12:00 -0300

    create and get categories passing on test

--- controllers/category.js
+++ controllers/category.js
@@ -3,7 +3,9 @@ const serviceCategory = require('../services/category');
 const createCategory = async (req, res) => {
     const { name } = req.body;
     const category = await serviceCategory.createCategory(name);
-    return res.status(category.status).json(category.message);
+
+    if (category.status === 201) return res.status(category.status).json(category.message);
+    return res.status(category.status).json({ message: category.message });
 };
 
 const getCategories = async (_req, res) => {
