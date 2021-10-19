eae0ab4beaf2698f1ca6201abd2b1cb4af699d56
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-19 16:45:47 -0300

    Created a getAll controller for Category

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -8,6 +8,15 @@ const create = async (req, res) => {
   return res.status(status).json(data);
 };
 
+const getAll = async (_req, res) => {
+  const { status, data, message } = await Category.getAll();
+
+  if (message) return res.status(status).json({ message });
+
+  return res.status(status).json(data);
+};
+
 module.exports = {
   create,
+  getAll,
 };
