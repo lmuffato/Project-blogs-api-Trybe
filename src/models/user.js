d83a9a4d56a47be54542bbf33b8cc2af9d64948e
Gabriel Ferreira <glucas709@gmail.com>
2021-10-19 21:09:28 -0300

    fix: requiriment 2

--- src/models/user.js
+++ src/models/user.js
@@ -1,17 +1,18 @@
-const User = (sequelize, DataTypes) => sequelize.define(
-  'User',
-  {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    displayName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    image: DataTypes.STRING,
-  },
-  {
-    timestamps: false,
-    underscored: true,
-    tableName: 'Users',
-  },
-);
+const User = (sequelize, DataTypes) =>
+  sequelize.define(
+    'User',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      displayName: { type: DataTypes.STRING, field: 'display_name' },
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'Users',
+    },
+  );
 
 module.exports = User;
