0fa999465e447e99b9cd770a52c6f14bc5a9ad5a
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-20 13:10:11 -0300

    Created the controller about getAll posts

--- controller/blogPostController.js
+++ controller/blogPostController.js
@@ -11,6 +11,15 @@ const create = async (req, res) => {
   return res.status(status).json(data);
 };
 
+const getAll = async (_req, res) => {
+  const { status, data, message } = await BlogPost.getAll();
+
+  if (message) return res.status(status).json({ message });
+
+  return res.status(status).json(data);
+};
+
 module.exports = {
   create,
+  getAll,
 };
