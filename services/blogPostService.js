bc0e6cebad7039920914f2a854c466c3c87d008b
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-20 11:56:38 -0300

    Created a service to create posts in the Database

---
+++ services/blogPostService.js
@@ -0,0 +1,34 @@
+const { BlogPost, Category } = require('../models');
+
+const Schema = require('../utils/schema');
+
+const create = async (body, userId) => {
+  const { error } = Schema.BlogPost.validate(body);
+  if (error) return { status: 400, message: error.details[0].message };
+
+  const { title, content, categoryIds } = body;
+
+  const checkCategories = await Category.findAll({ where: { id: categoryIds } });
+
+  if (checkCategories.length !== categoryIds.length) {
+    return { status: 400, message: '"categoryIds" not found' };
+  }
+
+  const post = await BlogPost
+    .create({ title, content, userId, published: new Date(), updated: new Date() });
+
+  console.log(post);
+
+  return { status: 201, data: post };
+};
+
+module.exports = {
+  create,
+};
+
+/* 
+  Entendi o uso de passar o id como parâmetro, pois somente um usuário com token
+  autenticado poderia fazer esse POST, e logo isso já diz que é um usuário no sistema.
+  E assim podendo checkar se o número de ids cadastrados batem com o que está no banco.
+  Entendi esses usos através do PR do Adelino Júnior https://github.com/tryber/sd-010-a-project-blogs-api/pull/10 
+*/
