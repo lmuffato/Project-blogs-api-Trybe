5a5509742da482685408014c707b01dc660598e0
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-20 11:59:15 -0300

    fix: change some values because when its testing the retur of values isnt equal, or break the test with empty promisses

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,9 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define('BlogPost', {
-    id: { type: DataTypes.INTEGER, primaryKey: true },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: { type: DataTypes.INTEGER, foreignKey: true },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
   },
   {
     timestamps: false,
