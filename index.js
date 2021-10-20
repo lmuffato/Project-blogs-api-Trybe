971728751f37a8dfe71603564b69fcc87e7abd9b
WillianMendes <willianmendesbr@gmail.com>
2021-10-19 21:07:08 -0300

    feat: add body parser

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
