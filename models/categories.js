b59e1d92d3f55f9266774a5a0e2de63751b5200d
Willian Alves <willian.alvesb65@gmail.com>
2021-10-20 17:37:32 -0300

    fix: lint

--- models/categories.js
+++ models/categories.js
@@ -1,23 +1,23 @@
-/* 'use strict';
 const {
-  Model
+  Model,
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  class Categories extends Model { */
+  class Categories extends Model { 
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-/*     static associate(models) {
+    static associate(/* models */) {
       // define association here
     }
-  };
+  }
   Categories.init({
-    fullName: DataTypes.STRING
+    fullName: DataTypes.STRING,
   }, {
     sequelize,
     modelName: 'Categories',
   });
   return Categories;
-}; */
+};
