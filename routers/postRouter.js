c140b7fbe7f118a660bb74e7b28b0547368a1969
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-10-20 19:08:36 -0300

    editPost router

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 
 const { validateJWT } = require('../middlewares/authMiddleware');
 const { validatePost } = require('../middlewares/postValidation');
+const { validateEditPost } = require('../middlewares/postEditValidation');
 
 const postController = require('../controllers/postController');
 
@@ -24,6 +25,13 @@ router.get(
   '/:id',
   validateJWT,
   postController.getbyId,
+);
+
+router.put(
+  '/:id',
+  validateJWT,
+  validateEditPost,
+  postController.editPost,
 );
 
 module.exports = router;
