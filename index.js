a0d263639ffe6536e829628c7ee9c1c6b291fc47
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-10-20 12:17:51 -0300

    Commit inicial - LETS GOOOOOOO!!

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
