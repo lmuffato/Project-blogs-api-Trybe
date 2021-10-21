33cede4f6473ba17ee744420fe01d9f6d665cbac
Willian Alves <willian.alvesb65@gmail.com>
2021-10-21 08:40:23 -0300

    Feat: req5

--- src/controller/categories.js
+++ src/controller/categories.js
@@ -2,6 +2,7 @@ const service = require('../service/categories');
 
 const createCategories = async (req, res) => {
   const { status, data, message } = await service.createCategories(req.body);
+  console.log(status, data, message);
   if (message) return res.status(status).json({ message });
 
   return res.status(status).json(data);
--- src/service/categories.js
+++ src/service/categories.js
@@ -1,11 +1,12 @@
-const Model = require('../../models/categories');
+const model = require('../../models/categories');
 const Joi = require('../middleware/joi');
 
 const createCategories = async (data) => { 
   const { error } = Joi.Categories.validate(data);
   if (error) return { status: 400, message: error.details[0].message };
 
-  const category = await Model.category(data);
+  const category = await model.Category(data);
+  console.log(category);
 
   return { status: 201, data: category };
 };
