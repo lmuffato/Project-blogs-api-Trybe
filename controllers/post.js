2196762f849f2cad40103d0038bd66453c813c13
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-10-21 13:51:16 -0300

    createPost removed

--- controllers/post.js
+++ controllers/post.js
@@ -1,22 +1,11 @@
-const { BlogPost, Category, User } = require('../models');
+const { BlogPost, User } = require('../models');
 
-const createPost = async (req, res) => {
-    const { title, content, categoryIds } = req.body;
+const createPost = async (_req, _res) => {
+/*     const { title, content, categoryIds } = req.body;
     const { userId } = req.user;
     const published = new Date();
-    const updated = new Date();
+    const updated = new Date(); */
 
-    // criar um post na tabela BlogPost
-    await BlogPost.create({ title, content, userId, published, updated });
-
-    const found = categoryIds.every(async (categoryId) => {
-        const result = await Category.findOne({ where: { id: categoryId } });
-        return result === categoryId;
-    });
-
-    const prom = Promise.all(found);
-
-    return res.status(200).send(prom);
 };
 
 const getPost = async (req, res) => {
