c85c8fc311e51c6f76d0041ae761a4f4d2078a94
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-10-21 20:10:42 -0300

    lint fix for evaluator

--- controllers/post.js
+++ controllers/post.js
@@ -1,11 +1,11 @@
 const { BlogPost, User, Category } = require('../models');
 
 const createPost = async (_req, _res) => {
-/*     const { title, content, categoryIds } = req.body;
+ /*    const { title, content, categoryIds } = req.body;
     const { userId } = req.user;
     const published = new Date();
-    const updated = new Date(); */
-
+    const updated = new Date();
+ */
 };
 
 const getPost = async (req, res) => {
