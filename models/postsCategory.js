8c236b417414e874dbdcfe8439b93bad36a173b0
igson <igson660@gmail.com>
2021-10-24 13:10:12 -0500

    adicina os aquivos que foram necessarios retirar para a resoluçao do conflito

---
+++ models/postsCategory.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, _DataTypes) => {
+  const PostsCategories = sequelize.define('PostsCategory', {}, {
+    tableName: 'PostsCategories', timestamps: false,
+  });
+
+  PostsCategories.associate = ({ Post, Category }) => {
+    Post.belongsToMany(Category, {
+      as: 'categories',
+      through: PostsCategories,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    Category.belongsToMany(Post, {
+      as: 'posts',
+      through: PostsCategories,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+  
+  return PostsCategories;
+};
