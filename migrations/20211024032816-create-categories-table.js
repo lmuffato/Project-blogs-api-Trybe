a023bbf03e9b89aa5a1ece89e128078f1b05e37e
Micaela Alves <micaela.alves93@gmail.com>
2021-10-24 13:26:53 -0300

    fix - renomeando migrations

--- migrations/20211024032816-create-categories-table.js
+++ migrations/20211024032816-create-categories-table.js
@@ -13,14 +13,6 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING
       },
-      // createdAt: {
-      //   allowNull: false,
-      //   type: Sequelize.DATE
-      // },
-      // updatedAt: {
-      //   allowNull: false,
-      //   type: Sequelize.DATE
-      // }
     });
   },
   down: async (queryInterface, Sequelize) => {
