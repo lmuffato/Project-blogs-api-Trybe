a289dcb7214ffbb272dc0ac1333679f1e0a4861f
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-25 09:13:45 -0300

    Created a function where receive the post id and return him

--- services/blogPostService.js
+++ services/blogPostService.js
@@ -33,9 +33,24 @@ const getAll = async () => {
   return { status: 200, data: posts };
 };
 
+const getById = async (id) => {
+  const post = await BlogPost.findOne({ 
+    where: { id },
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+   });
+
+  if (!post) return { status: 404, message: 'Post does not exist' };
+
+  return { status: 200, data: post };
+};
+
 module.exports = {
   create,
   getAll,
+  getById,
 };
 
 /* 
