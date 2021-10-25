0d987ffd32effffc2d4ab9213ee4daf9a76f0721
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-25 09:16:43 -0300

    Create in the Post controller the function getById

--- controller/blogPostController.js
+++ controller/blogPostController.js
@@ -2,8 +2,7 @@ const BlogPost = require('../services/blogPostService');
 
 const create = async (req, res) => {
   const { id } = req.user.data;
-  console.log(id);
-
+  
   const { status, data, message } = await BlogPost.create(req.body, id);
 
   if (message) return res.status(status).json({ message });
@@ -19,7 +18,17 @@ const getAll = async (_req, res) => {
   return res.status(status).json(data);
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const { status, data, message } = await BlogPost.getById(id);
+
+  if (message) return res.status(status).json({ message });
+
+  return res.status(status).json(data);
+};
+
 module.exports = {
   create,
   getAll,
+  getById,
 };
