dcadb01958fe569bfaed2e1622dec763849522be
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-10-25 12:16:33 -0300

    corrigido model category

--- models/category.js
+++ models/category.js
@@ -3,10 +3,5 @@ module.exports = (sequelize, DataTypes) => {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: DataTypes.STRING,
   }, { timestamps: false, primaryKey: true, tableName: 'Categories' }));
-  Category.associate = (models) => {
-    Category.hasMany(models.PostCategory, {
-      foreignKey: 'id', as: 'Categories',
-    });
-  };
   return Category;
 };
