7506e93f5c88eddf62b83456d216273b0c8a2ddd
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-10-24 22:14:52 -0300

    index refeito e organizado

--- index.js
+++ index.js
@@ -4,11 +4,42 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
+const users = require('./controllers/users');
+const categories = require('./controllers/categories');
+const posts = require('./controllers/posts');
+
+const {
+  nameValidation,
+  emailValidation,
+  passwordValidation,
+  findEmail,
+  tokenAuthentication,
+  validateName,
+  titleValidation,
+  contentValidation,
+  categoryIdValidation,
+} = require('./utils/validations');
+
 app.use(bodyParser.json());
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const PORT = process.env.PORT || 3000;
 
-app.post('/user', );
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
+
+app.post('/user', nameValidation, emailValidation, passwordValidation, findEmail, users.create);
+
+app.get('/user/:id', tokenAuthentication, users.getById);
+
+app.get('/user', tokenAuthentication, users.getAll);
+
+app.get('/categories', tokenAuthentication, categories.getAll);
+
+app.post('/categories', tokenAuthentication, validateName, categories.create);
+
+app.post('post', tokenAuthentication, titleValidation, 
+  contentValidation, categoryIdValidation, posts.create);
+
+app.get('post', tokenAuthentication, posts.getAll);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
