89a797d8e3edfa038327f6ee16b03027af164b30
jlfagundes <jlfagundes@gmail.com>
2021-10-25 13:20:07 -0300

    fix: model users

--- models/user.js
+++ models/user.js
@@ -4,6 +4,8 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
+  }, {
+    timestamps: false,
   });
   return User;
 };
