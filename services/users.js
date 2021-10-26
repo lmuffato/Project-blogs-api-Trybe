370103db3efde20f51c110917fa78351dc57143e
jlfagundes <jlfagundes@gmail.com>
2021-10-26 14:07:53 -0300

    fix: challenge 1 - service new user

--- services/users.js
+++ services/users.js
@@ -1,8 +1,12 @@
-const {User} = require('../models');
+const { User } = require('../models');
+const createToken = require('../auth/jwtFunctions');
 
-const create = async (displayNmae, email, password, image) => {
-  const newUser = await User.create(displayNmae, email, password, image);
-  return newUser;
+const create = async ({ displayNmae, email, password, image }) => {
+  await User.create(displayNmae, email, password, image);
+  // console.log('returno do model User:', teste);
+  const token = createToken.create(email);
+  // console.log('retorno da funcao create token:', token);
+  return token;
 };
 
 module.exports = {
