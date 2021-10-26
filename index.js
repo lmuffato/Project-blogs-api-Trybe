a97140bc0a35bdbaca10f7ec7f4cac99a478ecf5
jlfagundes <jlfagundes@gmail.com>
2021-10-26 11:10:37 -0300

    feat: challenge 1 create endpoint

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+// const routerUser = require('./routes/user');
+const controllerUsers = require('./controllers/users');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.post('/user', controllerUsers.create);
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
