382f6a96dbfc139b4e3e97b4ae80cf0e0f4a12b0
jlfagundes <jlfagundes@gmail.com>
2021-10-26 14:07:09 -0300

    fix: challenge 1 - return create user

--- controllers/users.js
+++ controllers/users.js
@@ -3,14 +3,15 @@ const serviceUsers = require('../services/users');
 const create = async (req, res) => {
   try {
     const { displayNmae, email, password, image } = req.body;
-    const newUser = await serviceUsers.create({
+    const token = await serviceUsers.create({
       displayNmae,
       email,
       password,
       image,
     });
 
-    res.status(201).json(newUser);
+    // console.log('o service esta retornando isso: ', newUser);
+    res.status(201).json({ token });
   } catch (e) {
     res.status(500).json({ message: 'Algo deu errado' });
   }
