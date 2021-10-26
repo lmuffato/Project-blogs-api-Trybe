fb89bc68a5728f673a17d391985ff86dec14b502
jlfagundes <jlfagundes@gmail.com>
2021-10-26 17:48:45 -0300

    fix: challenge 1 - service validate displayName and response for controller

--- services/users.js
+++ services/users.js
@@ -1,12 +1,21 @@
 const { User } = require('../models');
 const createToken = require('../auth/jwtFunctions');
+const validations = require('../validations/usersValidations');
+const { httpStatusCode } = require('../utils/errors');
 
-const create = async ({ displayNmae, email, password, image }) => {
-  await User.create(displayNmae, email, password, image);
-  // console.log('returno do model User:', teste);
-  const token = createToken.create(email);
-  // console.log('retorno da funcao create token:', token);
-  return token;
+const create = async ({ displayName, email, password, image }) => {
+  const validateDisplayName = validations.validateDisplayName(displayName);
+  if (validateDisplayName) return validateDisplayName;
+
+  try {
+    await User.create(displayName, email, password, image);
+    // console.log('returno do model User:', teste);
+    const token = createToken.create(email);
+    // console.log('retorno da funcao create token:', token);
+    return { status: httpStatusCode.ok, token };
+  } catch (e) {
+    return { status: httpStatusCode.badRequest, message: e.message };
+  }
 };
 
 module.exports = {
