2488604b39f41f3535ea279a16bf51ad552b0e3f
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-10-27 12:14:43 -0300

    :sparkles: Criação do model Category

--- migrations/20211027151148-create-category.js
+++ migrations/20211027151148-create-category.js
@@ -1,28 +1,19 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('Categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      displayName: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      image: {
+      name: {
         type: Sequelize.STRING
       }
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Categories');
   }
 };
