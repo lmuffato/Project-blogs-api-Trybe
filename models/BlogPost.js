02fb4f5395f7b9ef24fa8400ba1d609e0da47f0d
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-10-27 12:24:39 -0300

    :sparkles: Criação do model BlogPost

---
+++ models/BlogPost.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER, foreignKey: true,
+    },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, {
+    timestamps: false,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.Users, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  };
+
+  return BlogPost;
+};
