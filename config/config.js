119326b6517ebd4091de56597a84064393a0f930
Ederson S <edersonrodrigo31@gmail.com>
2021-10-30 11:51:04 -0300

    feat: sequelize connection

---
+++ config/config.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+    secret: process.env.JWT_SECRET,
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+    secret: process.env.JWT_SECRET,
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+    secret: process.env.JWT_SECRET,
+  },
+};
